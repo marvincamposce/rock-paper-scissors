d26f2681628c56352a5719f5ac6b8aa3f4457134
Marvin Campos <82715928+marvincamposce@users.noreply.github.com>
2024-04-18 21:35:54 -0600

    Create playRound function

--- script.js
+++ script.js
@@ -14,4 +14,24 @@ function getComputerChoice() {
   return choice;
 }
 
+const playerSelection = "Rock"
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log("You have a tie!")
+  }
+  else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    console.log(`You win! Rock beats scissors.`)
+  }
+  else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    console.log(`You win! Paper beats rock.`)
+  }
+  else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    console.log(`You win! Scissors beats paper.`)
+  }
+  else {
+    console.log(`You lose. ${computerSelection} beats ${playerSelection}`)
+  }
+}
 
